65f1a5e3f8702ed249b47a935efed34e9e20df52
pillaiag <pillaiag@mail.uc.edu>
2018-08-30 22:06:12 -0400

    Update prediction-validation.py

--- src/prediction-validation.py
+++ src/prediction-validation.py
@@ -1,7 +1,7 @@
 import pandas as pd
-actual = data = pd.read_csv('actual.txt', sep="|", header = None)
-predicted = pd.read_csv('predicted.txt', sep="|", header = None)
-windowFile = open("window.txt")
+actual = data = pd.read_csv('./input/actual.txt', sep="|", header = None)
+predicted = pd.read_csv('./input/predicted.txt', sep="|", header = None)
+windowFile = open("./input/window.txt")
 window = int(windowFile.read())
 windowFile.close()
 
@@ -16,7 +16,7 @@ for i, row in actual_df.iterrows():
 predicted['error'] = predicted.apply(lambda x : abs(stockDict[x[0]][x[1]]- x[2]), axis = 1)
 
 finalHour = predicted.iloc[- 1, 0]
-file = open("comparison.txt","w")
+file = open("./output/comparison.txt","w")
 for i in range(1, finalHour - (window-2)):
     error = round(predicted[(predicted[0] >= i) & (predicted[0] <= i + window - 1)]['error'].mean(), 2)
     file.write("" + str(i) + "|" + str(i+window-1) + "|" + str(error) + "\n")
